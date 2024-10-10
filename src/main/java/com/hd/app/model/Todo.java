88c1143d1739b621c035926d2b8ae3b60f56b17b
Thamizharasan K <thamizharasan.k@nibavlifts.com>
2024-10-10 16:01:58 +0530

    GET, POST method implemented and added the best practice in Entity

--- src/main/java/com/hd/app/model/Todo.java
+++ src/main/java/com/hd/app/model/Todo.java
@@ -5,23 +5,16 @@ import java.time.LocalDateTime;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.PreUpdate;
 
 @Entity
 public class Todo {
 
-    public Todo(int userId, String name, String description) {
-        this.userId = userId;
-        this.name = name;
-        this.description = description;
-        // this.dueDate = dueDate;
-        // this.isCompleted = isCompleted;
-    }
-
-    public Todo() {
-    }
-
     @Id
+    @GeneratedValue
     int id;
     @Column
     int userId;
@@ -29,14 +22,51 @@ public class Todo {
     String name;
     @Column
     String description;
-    // @Column
-    // LocalDate dueDate;
-    // @Column
-    // LocalDateTime createdAt;
-    // @Column
-    // LocalDateTime updatedAt;
-    // @Column
-    // boolean isCompleted;
+    @Column
+    LocalDate dueDate;
+    @Column
+    LocalDateTime createdAt;
+    @Column
+    LocalDateTime updatedAt;
+    @Column
+    boolean isCompleted;
+
+    public Todo() {
+        this.isCompleted = false;
+    }
+
+    public Todo(int userId, String name, String description, LocalDate dueDate) {
+        this(userId, name, description, dueDate, false);
+    }
+
+    public Todo(int userId, String name, String description, String dueDate) {
+
+        this(userId, name, description, LocalDate.parse(dueDate), false);
+    }
+
+    public Todo(int userId, String name, String description, LocalDate dueDate, Boolean isComplted) {
+        this.userId = userId;
+        this.name = name;
+        this.description = description;
+        this.dueDate = dueDate;
+        this.isCompleted = isComplted;
+    }
+
+    public Todo(int userId, String name, String description, String dueDate, Boolean isComplted) {
+        this(userId, name, description, LocalDate.parse(dueDate), isComplted);
+    }
+
+    @PrePersist
+    protected void onCreate() {
+        this.createdAt = LocalDateTime.now();
+    }
+
+    @PreUpdate
+    protected void onUpdate() {
+        this.updatedAt = LocalDateTime.now();
+    }
+
+    // Getter & Setter
 
     public int getId() {
         return id;
@@ -70,36 +100,36 @@ public class Todo {
         this.description = description;
     }
 
-    // public LocalDate getDueDate() {
-    // return dueDate;
-    // }
+    public LocalDate getDueDate() {
+        return dueDate;
+    }
 
-    // public void setDueDate(LocalDate dueDate) {
-    // this.dueDate = dueDate;
-    // }
+    public void setDueDate(LocalDate dueDate) {
+        this.dueDate = dueDate;
+    }
 
-    // public LocalDateTime getCreatedAt() {
-    // return createdAt;
-    // }
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
 
-    // public void setCreatedAt(LocalDateTime createdAt) {
-    // this.createdAt = createdAt;
-    // }
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
 
-    // public LocalDateTime getUpdatedAt() {
-    // return updatedAt;
-    // }
+    public LocalDateTime getUpdatedAt() {
+        return updatedAt;
+    }
 
-    // public void setUpdatedAt(LocalDateTime updatedAt) {
-    // this.updatedAt = updatedAt;
-    // }
+    public void setUpdatedAt(LocalDateTime updatedAt) {
+        this.updatedAt = updatedAt;
+    }
 
-    // public boolean isCompleted() {
-    // return isCompleted;
-    // }
+    public boolean isCompleted() {
+        return isCompleted;
+    }
 
-    // public void setCompleted(boolean isCompleted) {
-    // this.isCompleted = isCompleted;
-    // }
+    public void setCompleted(boolean isCompleted) {
+        this.isCompleted = isCompleted;
+    }
 
 }
---
+++ src/main/resources/schema.sql
@@ -0,0 +1,6 @@
+CREATE TABLE Todo (
+    id INT PRIMARY KEY AUTO_INCREMENT  ,
+    userId INT,
+    name VARCHAR(255),
+    description TEXT
+);
